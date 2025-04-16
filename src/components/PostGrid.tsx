f9460bdb6dba45c8ded519ad9a16338b73fa3c20
bbyik-k <kby00517@naver.com>
2025-04-16 19:46:52 +0900

    style: add styles for featuredPosts component

--- src/components/PostGrid.tsx
+++ src/components/PostGrid.tsx
@@ -5,7 +5,7 @@ type Props = {
 };
 export default function PostGrid({ posts }: Props) {
   return (
-    <ul className='flex max-w-5xl'>
+    <ul className='grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
       {posts.map((post) => (
         <li className='flex flex-col' key={post.path}>
           <PostCard post={post} />

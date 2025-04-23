e37a6abd5116a47c03318a199f97a10d9ed99cce
bbyik-k <kby00517@naver.com>
2025-04-23 11:26:54 +0900

    feat: add Contact page with form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,30 @@
+import ContactForm from '@/components/ContactForm';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
+
+const LINKS = [
+  {
+    icon: <AiFillGithub />,
+    url: 'https://github.com/bbyik-k',
+  },
+  {
+    icon: <AiFillLinkedin />,
+    url: 'https://www.linkedin.com/in/byeongik-kim-747881355/',
+  },
+];
 export default function ContactPage() {
-  return <p>연락하기!</p>;
+  return (
+    <section className='flex flex-col items-center'>
+      <h2 className='text-3xl font-bold by-2'>Contact Me</h2>
+      <p>ikbyeong.kim@gmail.com</p>
+      <ul className='flex gap-4 my-2'>
+        {LINKS.map((link, idx) => (
+          <a key={idx} href={link.url} target='_blank' rel='noopener noreferrer' className='text-4xl hover:text-yellow-400'>
+            {link.icon}
+          </a>
+        ))}
+      </ul>
+      <h2 className='text-3xl font-bold my-8'>Or Send me an email</h2>
+      <ContactForm />
+    </section>
+  );
 }
---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { ChangeEvent, FormEvent, useState } from 'react';
+import Banner, { BannerData } from './Banner';
+
+type Form = {
+  from: string;
+  subject: string;
+  message: string;
+};
+export default function ContactForm() {
+  const [form, setForm] = useState<Form>({ from: '', subject: '', message: '' });
+  const [banner, setBanner] = useState<BannerData | null>(null);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(form);
+    setBanner({ message: '전송에 성공하였습니다!', state: 'success' });
+    setTimeout(() => {
+      // setBanner(null);
+    }, 3000);
+  };
+
+  return (
+    <section className='w-full max-w-md '>
+      {banner && <Banner banner={banner} />}
+      <form onSubmit={onSubmit} className='w-full flex flex-col gap-2 my-4 p-4 bg-slate-700 rounded-2xl'>
+        <label className='font-semibold' htmlFor='from'>
+          Your Email
+        </label>
+        <input className='bg-slate-500' type='email' id='from' name='from' required autoFocus value={form.from} onChange={onChange} />
+        <label className='font-semibold' htmlFor='subject'>
+          Subject
+        </label>
+        <input className='bg-slate-500' type='text' id='subject' name='subject' required value={form.subject} onChange={onChange} />
+        <label className='font-semibold' htmlFor='message'>
+          Message
+        </label>
+        <textarea className='bg-slate-500' rows={10} id='message' name='message' required value={form.message} onChange={onChange} />
+        <button className='bg-cyan-700 font-bold hover:bg-cyan-600'>Submit</button>
+      </form>
+    </section>
+  );
+}
